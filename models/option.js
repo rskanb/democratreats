dd4fd8662db31aef2d311f08cb51ffd31a588739
Jordan Larios <jolarios96@gmail.com>
2019-01-08 14:08:09 -0800

    moved options.js from public folder to models

---
+++ models/option.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, DataTypes) {
+    var Option = sequelize.define("Option", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 20]
+            }
+        },
+        vote: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        }
+    });
+
+    Option.associate = function (models) {
+        Option.belongsTo(models.Poll, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Option;
+};
