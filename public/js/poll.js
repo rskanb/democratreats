726bc8a8755b47f356d3c4b6fa4f215cee4dcc0d
Jordan Larios <jolarios96@gmail.com>
2019-01-08 13:56:17 -0800

    notably: option.js for Option table associations

---
+++ public/js/poll.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 20]
+            }
+        },
+        email: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        admin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+
+    });
+    return User;
+};
