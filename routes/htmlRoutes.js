daaf7112e0eca2d535121d44e64d2f433f628666
Jordan Larios <jolarios96@gmail.com>
2019-01-09 07:23:20 -0800

    A base admin view of the main page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,11 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-      res.render("index", {
-        msg: "Welcome!",
-      });
+  app.get("/", function (req, res) {
+    res.render("index", {
+      msg: "Welcome!",
+    });
   });
 
   // app.get("/", function(req, res) {
@@ -17,21 +17,27 @@ module.exports = function(app) {
   //   });
   // });
 
-  app.get("/login", function(req, res) {
-      res.render("login", {
-        msg: "Welcome!"
-      });
+  app.get("/login", function (req, res) {
+    res.render("login", {
+      msg: "Welcome!"
+    });
   });
 
-  app.get("/employee", function(req, res) {
+  app.get("/employee", function (req, res) {
     res.render("employee", {
       msg: "Welcome!"
     });
-});
+  });
+
+  app.get("/admin", function (req, res) {
+    res.render("admin", {
+      msg: "Welcome!"
+    });
+  });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -39,7 +45,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
