5195927b4430e36844e45b261ab382fa86e43d3e
Jordan Larios <jolarios96@gmail.com>
2019-01-11 11:24:16 -0800

    delete button deletes poll options now too

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,10 @@ var db = require("../models");
 var passport = require("../config/passport");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 const url = require("url");
-module.exports = function(app) {
+module.exports = function (app) {
 
   // {successRedirect: "/home", failureRedirect:"/"}
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
 
     res.redirect("/home")
   });
@@ -39,46 +39,51 @@ module.exports = function(app) {
   });
 
   //Get all poll
-  app.get("/api/poll", function(req,res){
+  app.get("/api/poll", function (req, res) {
     // console.log(req);
     console.log("api getpoll route hit")
-    db.Poll.findAll({}).then(function(dbpoll) {
+    db.Poll.findAll({}).then(function (dbpoll) {
       res.json(dbpoll);
-  })
+    })
   });
 
   //Delete Poll Baserd on ID Selected 
-  app.delete("/api/poll/:id", isAuthenticated, function(req, res){
+  app.delete("/api/poll/:id", isAuthenticated, function (req, res) {
     var delId = parseInt(req.params.id);
+    db.Option.destroy({
+      where: {
+        Pollid: delId
+      }
+    });
     db.Poll.destroy({
       where: {
         id: delId
       }
-    }).then(function(dbPost) {
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
-    });
+  });
 
-    app.post("/api/edit/:id", isAuthenticated, function(req, res){
-      var reqId = req.params.id;
-      if(req.user.admin){
-        db.Poll.findOne({ where: { id: reqId } }).then(function(poll) {
-          var editObject = {
-            id: poll.dataValues.id,
-            name: poll.dataValues.name,
-            description: poll.dataValues.description
-          }
-          res.json(editObject);
-          res.end();
-        })
-        //res.json(reqId);
-        //res.redirect(url.format({pathname:"/admin",query:{reqId}}));
-        //res.redirect("/admin");
-        //console.log(url.format({pathname:"/admin",query: {reqId}}));
-    }else {
-        res.redirect("*")
-     }
-    });
+  app.post("/api/edit/:id", isAuthenticated, function (req, res) {
+    var reqId = req.params.id;
+    if (req.user.admin) {
+      db.Poll.findOne({ where: { id: reqId } }).then(function (poll) {
+        var editObject = {
+          id: poll.dataValues.id,
+          name: poll.dataValues.name,
+          description: poll.dataValues.description
+        }
+        res.json(editObject);
+        res.end();
+      })
+      //res.json(reqId);
+      //res.redirect(url.format({pathname:"/admin",query:{reqId}}));
+      //res.redirect("/admin");
+      //console.log(url.format({pathname:"/admin",query: {reqId}}));
+    } else {
+      res.redirect("*")
+    }
+  });
 
 
   app.get("/logout", function (req, res) {
@@ -89,7 +94,7 @@ module.exports = function(app) {
 
 
 
-app.put("/api/update", isAuthenticated, function(req,res){
+  app.put("/api/update", isAuthenticated, function (req, res) {
     console.log("api update put route hit");
     console.log(req.body);
     db.Poll.update(
@@ -98,9 +103,9 @@ app.put("/api/update", isAuthenticated, function(req,res){
         where: {
           id: req.body.id
         }
-      }).then(function(dbPost) {
-      res.json(dbPost);
-    });
+      }).then(function (dbPost) {
+        res.json(dbPost);
+      });
   });
 
 
