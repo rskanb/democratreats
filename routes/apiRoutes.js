3c90971b7f1a2e48ad2dcba89bf4db8ecf957e44
Jordan Larios <jolarios96@gmail.com>
2019-01-11 13:47:46 -0800

    fixed Members (admin side) bug

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -47,63 +47,26 @@ module.exports = function (app) {
     });
   });
 
-    // Create a new VOTE
-    app.post("/api/votes", isAuthenticated, function (req, res) {
-      console.log(req.body);
-      db.Vote.create(req.body).then(function (dbVote) {
-        res.json(dbVote);
-      });
-    });
-
-
-    // Create a new REQUEST
-    app.post("/api/requests", isAuthenticated, function (req, res) {
-      console.log(req.body);
-      db.Request.create(req.body).then(function (dbRequest) {
-        res.json(dbRequest);
-        console.log("request API hit");
-      });
+  // Create a new REQUEST
+  app.post("/api/requests", function (req, res) {
+    console.log(req.body);
+    db.Request.create(req.body).then(function (dbRequest) {
+      res.json(dbRequest);
+      console.log("request API hit");
     });
   });
 
   //Get all poll
-  app.get("/api/poll", isAuthenticated, function (req, res) {
 
+  app.get("/api/poll", isAuthenticated, function (req, res) {
     // console.log(req);
     console.log("api getpoll route hit")
-    console.log(" use id "+ req.user.id);
-    var userId = req.user.id;
     db.Poll.findAll({
       include: [db.Option]
-
-    }).then(function(dbpoll) {
-      var newUserData = dbpoll.push({"userLoginId":  userId});
-      console.log(newUserData);
+    }).then(function (dbpoll) {
       res.json(dbpoll);
     })
   });
-
-  //Get all Request
-  app.get("/api/request", isAuthenticated, function(req,res){
-    // console.log(req);
-    console.log("api get request route hit")
-    db.Request.findAll({}).then(function(dbrequest) {
-      res.json(dbrequest);
-    })
-  });
-
-  //Delete Requests by Admin only
-  app.delete("/api/request/:id", isAuthenticated, function (req, res) {
-    var delReqId = parseInt(req.params.id);
-    console.log("delete request route ended")
-    db.Request.destroy({
-      where: {
-        id: delReqId
-      }
-    }).then(function (dbPost) {
-      res.json(dbPost);
-    });
-  });   //Delete Request Function end 
 
   //Delete Poll Baserd on ID Selected 
   app.delete("/api/poll/:id", isAuthenticated, function (req, res) {
@@ -120,7 +83,7 @@ module.exports = function (app) {
     }).then(function (dbPost) {
       res.json(dbPost);
     });
-  });   //Delete Poll Function end
+  });
 
   app.post("/api/edit/:id", isAuthenticated, function (req, res) {
     var reqId = req.params.id;
